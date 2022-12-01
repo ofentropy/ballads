5248f228e816e1ccb04e5ae858cd3778da0f896a
ofentropy <40444915+ofentropy@users.noreply.github.com>
2022-12-01 12:56:20 -0800

    Add graph method

--- triple_cnn/tc_util/importutil.py
+++ triple_cnn/tc_util/importutil.py
@@ -1,4 +1,5 @@
 import csv
+import matplotlib.pyplot as plt
 
 def get_labels_from_text(labels_path):
     with open(labels_path, 'r') as f:
@@ -19,3 +20,19 @@ def get_img_labels_from_csv(csv_path, key):
             labels = labels.split(";")
             urls_to_label[url] = labels
     return urls_to_label
+
+def create_graph_from_urls_to_label(urls_to_label, key):
+    label_counts = {}
+    for _, labels in urls_to_label.items():
+        for label in labels:
+            label_counts[label] = label_counts.get(label, 0) + 1
+    names = label_counts.keys()
+    values = label_counts.values()
+    
+    total = sum(values)
+    percentages = [v/total for v in values]
+    print(percentages)
+
+    plt.bar(range(len(label_counts)), values, tick_label=names)
+    plt.savefig(key, format="png")
+    #plt.show()
